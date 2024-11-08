d11ee42ebcc1e81ff68ada344b7eb96dbfc2493d
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2024-11-07 21:57:01 -0300

    Aula 07/11/2024 Comandos Banco de Dados

--- concessionaria.sql
+++ concessionaria.sql
@@ -62,3 +62,49 @@ CREATE TABLE vendas(
 SELECT * FROM veiculos
 INSERT INTO fabricantes(nome, pais_origem,ano_fundacao) VALUES ('SUBARU','JAPAO','1953')
 INSERT INTO veiculos(chassi,modelo,ano_fabricacao,cor,quilometragem,preco_venda,id_fabricante) VALUES ('4529874012A5C8ABCE5','SKYLINE-R34','1998','AZUL-MARINHO','0','200.950','2b42e816-bf90-4c93-bf36-a39e407c2f7d')
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+SELECT * FROM vendedores
+
+SELECT MAX(preco_venda)FROM veiculos
+SELECT MAX(preco_venda) AS maior_preco FROM veiculos
+
+SELECT MAX(preco_venda)FROM veiculos
+SELECT MIN(preco_venda) AS menor_preco FROM veiculos
+
+SELECT MAX(preco_venda), MIN(preco_venda)FROM veiculos
+SELECT MAX(preco_venda)AS maior_preco, MIN(preco_venda) AS menor_preco FROM veiculos
+
+SELECT * FROM veiculos WHERE preco_venda = 200.95
+
+--FAZ AS MÉDIAS
+SELECT AVG(preco_venda) FROM veiculos
+SELECT AVG(quilometragem) AS media_km FROM veiculos;
+
+--ROUD ARRUMOU AS CASAS DECIMAIS FAZENDO A MÉDIA 1º DPS O RESTO
+SELECT ROUND(AVG(quilometragem),2)FROM veiculos
+
+SELECT SUM(preco_venda) FROM veiculos
+SELECT SUM(preco_venda)AS TOTAL FROM veiculos
+--SOMANDO O VALOR DE ALGO ESPECIFICO
+SELECT SUM(preco_venda)FROM veiculos WHERE cor = 'BRANCO'
+SELECT SUM(preco_venda)FROM veiculos WHERE ano_fabricacao >= 2010
+
+SELECT * FROM vendas
+SELECT SUM(valor_total)FROM vendas WHERE data_venda BETWEEN '2021-01-10' AND '2024-11-07'
+--maior pro menor
+SELECT * FROM vendas ORDER BY valor_total DESC
+
+--menor pro maior
+SELECT * FROM clientes ORDER BY nome ASC
+
+--agrupar por um campo... nao pode usar "*" !!!!
+SELECT metodo_pagamento, COUNT(id) FROM vendas GROUP BY metodo_pagamento
+
+--LIKE significado "como"....
+SELECT * FROM vendedores WHERE nome LIKE '%nho%'
+SELECT * FROM vendedores WHERE email LIKE '%@%'
+
+SELECT * FROM veiculos
+SELECT * FROM veiculos WHERE modelo LIKE '_____'
+SELECT * FROM veiculos WHERE modelo LIKE '___R%'
